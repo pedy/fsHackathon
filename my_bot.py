d23774edabcff1f7b8cc3e5ba3b71172fb942310
Pedram Shamkhali <pshamkhali@gmail.com>
2019-02-01 15:54:46 +0330

    - Parse non-match strings to the most similar key - Space separate currency key pairs in JSON to be more human readable

--- my_bot.py
+++ my_bot.py
@@ -1,5 +1,19 @@
 import discord
 import json
+from difflib import SequenceMatcher
+
+def return_most_similar(inputStr):
+    with open('sampleRates.json') as json_data:
+        d = json.load(json_data)
+
+        pair_similarity_ratio = {}
+        for key in d['exchangeRates']:
+            pair_similarity_ratio[key] = SequenceMatcher(None, key, inputStr).ratio()
+        
+        most_similar = max(pair_similarity_ratio, key=pair_similarity_ratio.get)
+        #print(pair_similarity_ratio)
+        #print("most similar", most_similar)
+        return most_similar
 
 TOKEN = 'NTQwNzczMzIwNzI0MDU0MDI0.DzWB7A.EUTm3Ok7tGafxa_2eLbZXKv0arM'
 
@@ -10,17 +24,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    
-    with open('sampleRates.json') as json_data:
-        d = json.load(json_data)
-
-        try:
-            result = d['exchangeRates'][message.content]
-            await client.send_message(message.channel, result)
-        except:
-            error_msg = "Valid currency codes: " + str(d['validCurrencySymbols'])
-                        + "Please use symbols pairs like this: AUDUSD, USDAUD, ...\n"
-            await client.send_message(message.channel, error_msg)
+    result = return_most_similar(message.content.upper())
+    await client.send_message(message.channel, result)
 
 @client.event
 async def on_ready():
@@ -30,3 +35,4 @@ async def on_ready():
     print('------')
 
 client.run(TOKEN)
+
